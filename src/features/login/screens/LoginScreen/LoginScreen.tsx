1a361d4fd52526b8c3914f14b1a0c7bd58acbd48
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-04-11 02:31:55 +0200

    feat: add login screen

---
+++ src/features/login/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,62 @@
+import { Button, Checkbox, Form, Input, Card } from "antd";
+import { useNavigate } from "react-router-dom";
+
+const LoginScreen = () => {
+  const navigate = useNavigate();
+
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+    if (values.username === "arnaf" && values.password === "4321") {
+      navigate("/");
+    }
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <Card style={{ width: 600 }}>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+};
+export default LoginScreen;
