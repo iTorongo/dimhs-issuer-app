d8f6d9c0209bec6396fafda95b5814a8e31f46aa
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-05-01 23:59:45 +0200

    feat: introduce more api endpoints

--- src/api/services.ts
+++ src/api/services.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const HOSPITAL_API_HOST = "http://localhost:8021/";
+
 const apiClient = axios.create({
-  baseURL: "http://localhost:8021",
+  baseURL: HOSPITAL_API_HOST,
   headers: {
     "Content-type": "application/json",
   },
@@ -22,6 +24,10 @@ export const getConnectionById = (connectionId?: string) => {
   return apiClient.get(`/connections/${connectionId}`);
 };
 
+export const acceptConnection = (connectionId?: string) => {
+  return apiClient.post(`/connections/${connectionId}/accept-request`);
+};
+
 export const createConnectionInvitation = (requestBody: any) => {
   return apiClient.post("/connections/create-invitation", requestBody);
 };
@@ -30,7 +36,11 @@ export const getSchemas = () => {
   return apiClient.get("/schemas/created");
 };
 
-export const getSchema = (schemaId: string) => {
+export const createSchema = (requestBody: any) => {
+  return apiClient.post("/schemas", requestBody);
+};
+
+export const getSchemaDetails = (schemaId: string) => {
   return apiClient.get(`/schemas/${schemaId}`);
 };
 
@@ -38,8 +48,12 @@ export const getCredDefs = () => {
   return apiClient.get("/credential-definitions/created");
 };
 
-export const getCredDef = (credDefId: string) => {
+export const getCredDefDetails = (credDefId: string) => {
   return apiClient.get(`/credential-definitions/${credDefId}`);
+};
+
+export const createCredentialDefinition = (requestBody: any) => {
+  return apiClient.post("/credential-definitions", requestBody);
 };
 
 export const testGetEndpoints = () => {
@@ -48,6 +62,22 @@ export const testGetEndpoints = () => {
   );
 };
 
+export const getIssueCredentials = () => {
+  return apiClient.get("/issue-credential/records");
+};
+
+export const getPublicDIDFromWallet = () => {
+  return apiClient.get("/wallet/did/public");
+};
+
 export const issueCredentialSend = (requestBody: any) => {
   return apiClient.post("/issue-credential/send", requestBody);
+};
+
+export const createCredential = (requestBody: any) => {
+  return apiClient.post("/issue-credential/create", requestBody);
+};
+
+export const sendCredentialOffer = (requestBody: any) => {
+  return apiClient.post("/issue-credential/send-offer", requestBody);
 };
